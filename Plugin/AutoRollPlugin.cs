3fab8b681a9a11ab326d0674ec02df3ec7a6bd77
Lord Ashes <webtable@gmail.com>
2021-10-17 15:00:47 -0400

    Version 1.0.2
    Initial version with extra randomness on the rolls and checks to ignore manually added dice.

---
+++ Plugin/AutoRollPlugin.cs
@@ -0,0 +1,82 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using Bounce.Singletons;
+using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+namespace LordAshes
+{
+    [BepInPlugin(Guid, Name, Version)]
+    public partial class AutoRollPlugin : BaseUnityPlugin
+    {
+        // Plugin info
+        public const string Name = "Auto Roll Plug-In";
+        public const string Guid = "org.lordashes.plugins.autoroll";
+        public const string Version = "1.0.2.0";
+
+        private List<int> rollIds = new List<int>();
+
+        /// <summary>
+        /// Function for initializing plugin
+        /// This function is called once by TaleSpire
+        /// </summary>
+        void Awake()
+        {
+            // Not required but good idea to log this state for troubleshooting purpose
+            UnityEngine.Debug.Log("Auto Roll Plugin: Active. Dice added via talespire://dice prtotocol will be auto added and rolled.");
+
+            callbackRollReady = AutoRoll;
+            callbackRollResult = AutoHide;
+            var harmony = new Harmony(Guid);
+            harmony.PatchAll();
+
+            Utility.PostOnMainPage(this.GetType());
+        }
+
+        /// <summary>
+        /// Function for determining if view mode has been toggled and, if so, activating or deactivating Character View mode.
+        /// This function is called periodically by TaleSpire.
+        /// </summary>
+        void Update()
+        {
+        }
+
+        public void AutoRoll(int rollId)
+        {
+            rollIds.Add(rollId);
+            StartCoroutine((IEnumerator)DelayedDiceSetRoll(rollId));
+        }
+
+        public void AutoHide()
+        {
+            float hideDelay = Config.Bind("Setting", "Hide Delay In Seconds", 3f).Value;
+            if (hideDelay >= 0)
+            {
+                StartCoroutine((IEnumerator)DelayedDiceSetHide(hideDelay));
+            }
+        }
+
+        private IEnumerator DelayedDiceSetRoll(int rollId)
+        {
+            yield return new WaitForSeconds(0.500f);
+            DiceManager dm = GameObject.FindObjectOfType<DiceManager>();
+            Debug.Log("Auto Roll Plugin: Rolling Dice");
+            float extraBounceHeight = Config.Bind("Setting", "Extra Bounce Height", 0.0f).Value;
+            if (extraBounceHeight > 0.0f) { dm.GatherDice(SimpleSingletonBehaviour<CameraController>.Instance.transform.position + (extraBounceHeight * Vector3.up), rollId); }
+            dm.ThrowDice(rollId);
+        }
+
+        private IEnumerator DelayedDiceSetHide(float delayHide)
+        {
+            yield return new WaitForSeconds(delayHide);
+            DiceManager dm = GameObject.FindObjectOfType<DiceManager>();
+            Debug.Log("Auto Roll Plugin: Clearing Dice");
+            dm.ClearAllDice(rollIds.ElementAt(0));
+            rollIds.RemoveAt(0);
+        }
+    }
+}
---
+++ Plugin/Patches/Patches.cs
@@ -0,0 +1,230 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace LordAshes
+{
+    public partial class AutoRollPlugin : BaseUnityPlugin
+    {
+        public static Action<int> callbackRollReady = null;
+        public static Action callbackRollResult = null;
+
+        private static System.Random random = new System.Random();
+
+        [HarmonyPatch(typeof(UIDiceTray), "SetDiceUrl")]
+        public static class Patches
+        {
+            public static bool Prefix(DiceRollDescriptor rollDescriptor)
+            {
+                return true;
+            }
+
+            public static void Postfix(DiceRollDescriptor rollDescriptor)
+            {
+                DiceManager dm = GameObject.FindObjectOfType<DiceManager>();
+                foreach (DiceGroupDescriptor dgd in rollDescriptor.Groups)
+                {
+                    if (dgd.Name != "")
+                    {
+                        Debug.Log("Auto Roll Plugin: Spawning Dice");
+                        foreach (DiceDescriptor dd in dgd.Dice)
+                        {
+                            UIDiceTray dt = GameObject.FindObjectOfType<UIDiceTray>();
+                            bool saveSetting = (bool)PatchAssistant.GetField(dt, "_buttonHeld");
+                            PatchAssistant.SetField(dt, "_buttonHeld", true);
+                            dt.SpawnDice();
+                            PatchAssistant.SetField(dt, "_buttonHeld", saveSetting);
+                        }
+                    }
+                    else
+                    {
+                        Debug.Log("Auto Roll Plugin: Ignoring Manually Added Dice");
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(DiceManager), "CreateLocalRoll")]
+        public static class PatchCreateLocalRoll
+        {
+            public static bool Prefix(DiceRollDescriptor rollDescriptor, bool isGmRoll)
+            {
+                return true;
+            }
+
+            public static void Postfix(DiceRollDescriptor rollDescriptor, bool isGmRoll, ref int __result)
+            {
+                string name = "";
+                if(rollDescriptor.Groups!=null)
+                {
+                    if(rollDescriptor.Groups[0].Name!=null)
+                    {
+                        name = Convert.ToString(rollDescriptor.Groups[0].Name);
+                    }
+                }
+                if (name != "")
+                {
+                    Debug.Log("Auto Roll Plugin: Dice Set '" + name + "' Ready ");
+                    if (callbackRollResult != null) { callbackRollReady(__result); }
+                }
+                else
+                {
+                    Debug.Log("Auto Roll Plugin: Manual Dice Set Is Ready");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(DiceManager), "RPC_DiceResult")]
+        public static class PatchDiceResults
+        {
+            public static bool Prefix(bool isGmOnly, byte[] diceListData, PhotonMessageInfo msgInfo)
+            {
+                return true;
+            }
+
+            public static void Postfix(bool isGmOnly, byte[] diceListData, PhotonMessageInfo msgInfo)
+            {
+                if (callbackRollResult != null) { callbackRollResult(); }
+            }
+        }
+
+        [HarmonyPatch(typeof(Die), "Spawn")]
+        public static class PatchDieSpawn
+        {
+            public static bool Prefix(string resource, float3 pos, quaternion rot, int rollId, byte groupId, bool gmOnlyDie)
+            {
+                return false;
+            }
+
+            public static void Postfix(string resource, float3 pos, quaternion rot, int rollId, byte groupId, bool gmOnlyDie, ref Die __result)
+            {
+                object[] data = new object[]
+                {
+                    rollId,
+                    groupId,
+                    gmOnlyDie
+                };
+                Die component = PhotonNetwork.Instantiate(resource, pos, rot, 0, data).GetComponent<Die>();
+                PatchAssistant.UseMethod(component, "Init", new object[] { rollId, groupId, gmOnlyDie }); // component.Init(rollId, groupId, gmOnlyDie);
+                Vector3 orientation = new Vector3(random.Next(0, 180), random.Next(0, 180), random.Next(0, 180));
+                Debug.Log("Auto Roll Plugin: Randomizing Die Starting Orientation ("+orientation.ToString()+")");
+                component.transform.rotation = Quaternion.Euler(orientation);
+                __result = component;
+            }
+        }
+
+        public static class PatchAssistant
+        {
+            public static object GetProperty(object instance, string propertyName)
+            {
+                Type type = instance.GetType();
+                foreach (PropertyInfo modifier in type.GetRuntimeProperties())
+                {
+                    if (modifier.Name.Contains(propertyName))
+                    {
+                        return modifier.GetValue(instance);
+                    }
+                }
+                foreach (PropertyInfo modifier in type.GetProperties())
+                {
+                    if (modifier.Name.Contains(propertyName))
+                    {
+                        return modifier.GetValue(instance);
+                    }
+                }
+                return null;
+            }
+
+            public static void SetProperty(object instance, string propertyName, object value)
+            {
+                Type type = instance.GetType();
+                foreach (PropertyInfo modifier in type.GetRuntimeProperties())
+                {
+                    if (modifier.Name.Contains(propertyName))
+                    {
+                        modifier.SetValue(instance, value);
+                        return;
+                    }
+                }
+                foreach (PropertyInfo modifier in type.GetProperties())
+                {
+                    if (modifier.Name.Contains(propertyName))
+                    {
+                        modifier.SetValue(instance, value);
+                        return;
+                    }
+                }
+            }
+            public static object GetField(object instance, string fieldName)
+            {
+                Type type = instance.GetType();
+                foreach (FieldInfo modifier in type.GetRuntimeFields())
+                {
+                    if (modifier.Name.Contains(fieldName))
+                    {
+                        return modifier.GetValue(instance);
+                    }
+                }
+                foreach (FieldInfo modifier in type.GetFields())
+                {
+                    if (modifier.Name.Contains(fieldName))
+                    {
+                        return modifier.GetValue(instance);
+                    }
+                }
+                return null;
+            }
+
+            public static void SetField(object instance, string fieldName, object value)
+            {
+                Type type = instance.GetType();
+                foreach (FieldInfo modifier in type.GetRuntimeFields())
+                {
+                    if (modifier.Name.Contains(fieldName))
+                    {
+                        modifier.SetValue(instance, value);
+                        return;
+                    }
+                }
+                foreach (FieldInfo modifier in type.GetFields())
+                {
+                    if (modifier.Name.Contains(fieldName))
+                    {
+                        modifier.SetValue(instance, value);
+                        return;
+                    }
+                }
+            }
+
+            public static object UseMethod(object instance, string methodName, object[] parameters)
+            {
+                Type type = instance.GetType();
+                foreach (MethodInfo modifier in type.GetRuntimeMethods())
+                {
+                    if (modifier.Name.Contains(methodName))
+                    {
+                        return modifier.Invoke(instance, parameters);
+                    }
+                }
+                foreach (MethodInfo modifier in type.GetMethods())
+                {
+                    if (modifier.Name.Contains(methodName))
+                    {
+                        return modifier.Invoke(instance, parameters);
+                    }
+                }
+                return null;
+            }
+        }
+    }
+}
